65819cb513b0d72f5b9dbb906a61ad90e366a17a
elsell <john.sell@mail.wlc.edu>
2022-01-20 17:49:46 -0600

    Initial code commit.

---
+++ MinecraftOnlinePlayersOverlay.py
@@ -0,0 +1,228 @@
+import sys
+import argparse
+import configparser
+from mcserver_connector import MinecraftConnector
+from PIL import Image, ImageDraw, ImageFont
+import time
+import os
+import logging
+
+
+class MinecraftOnlinePlayersOverlay:
+    def __init__(self, image_output_dir: str, minecraft_server_ip: str, **kwargs):
+        """[summary]
+
+        Args:
+            * image_output_dir (str): The directory in which the player list image file will be saved.
+            * minecraft_server_ip (str):  The IP address of the Minecraft server to poll.
+
+        KeywordArguments:
+            * ``minecraft_server_port`` [``int``] -- (default: 25565) The port corresponding to ``minecraft_server_ip``
+
+            * ``draw_shadow`` [``bool``] -- (default: True) Should a shadow be drawn below the player names.
+
+            * ``image_name`` [``str``] -- (default: ``online_players.png``) The name of the generated image.
+
+            * ``vertical_padding`` [``int``] -- (default: ``12``) Pixels between player head images.
+
+            * ``refresh_every_seconds`` [``int``] -- (default: ``3``) How often to refresh the player list in seconds.
+        """
+        self._log = logging.getLogger(__name__ + ".MOPO")
+
+        default_kwargs = {
+            "minecraft_server_port": 25565,
+            "draw_shadow": True,
+            "image_name": "online_players.png",
+            "vertical_padding": 12,
+            "refresh_every_seconds": 3,
+        }
+
+        # Combine kwargs with default to fill missing gaps
+        kwargs = {**default_kwargs, **kwargs}
+
+        self._minecraft_server_ip = minecraft_server_ip
+        self._server_port = int(kwargs["minecraft_server_port"])
+
+        self._mcserver = MinecraftConnector(minecraft_server_ip, self._server_port)
+        self._image_output_dir = image_output_dir
+        self._player_image_name = kwargs["image_name"]
+        self._draw_shadow = kwargs["draw_shadow"]
+        self._vertical_padding = int(kwargs["vertical_padding"])
+        self._refresh_every_seconds = int(kwargs["refresh_every_seconds"])
+
+        self._print_init()
+
+    def _print_init(self):
+        self._log.info("Initializing MinecraftOnlinePlayersOverlay (MOPO)")
+        items = {
+            "Image Output Directory": self._image_output_dir,
+            "Output Image Filename": self._player_image_name,
+            "Minecraft Server IP": self._minecraft_server_ip,
+            "Minecraft Server Port": self._server_port,
+            "Draw Shadow": self._draw_shadow,
+            "Vertical Padding": self._vertical_padding,
+            "Refresh Rate (seconds)": self._refresh_every_seconds,
+        }
+        for item in items:
+            self._log.info(
+                "{}{:<30}{}".format(" " * 4, "{}:".format(item), items[item])
+            )
+
+    def _save_image(self, image, image_name):
+        image.save(os.path.join(self._image_output_dir, image_name))
+
+    def _build_player_image_board(self):
+        players = self._mcserver._get_online_players()
+
+        if len(players) > 0:
+            names = [p["name"] for p in players]
+            images = [Image.open(i) for i in [p["image"] for p in players]]
+            widths, heights = zip(*(i.size for i in images))
+
+            # These seem backwards because we are making
+            # a vertical image.
+            total_height = sum(widths) + (self._vertical_padding * len(images))
+            total_width = max(heights)
+
+            self._log.debug(
+                (
+                    "Image Height (without text): {}".format(total_height),
+                    "Image Width (without text): {}".format(total_width),
+                )
+            )
+
+            combined_images = Image.new("RGBA", (total_width, total_height))
+
+            y_offset = 0
+            for image in images:
+                combined_images.paste(image, (0, y_offset))
+                y_offset += image.size[0] + self._vertical_padding
+
+            return self._draw_player_names_on_image(
+                combined_images, names, total_width, self._vertical_padding
+            )
+        else:
+            return None
+
+    def _draw_player_names_on_image(self, image, names, head_height, padding):
+        width, height = image.size
+        name_width = 1920
+        name_right_padding = 15
+
+        named_image = Image.new("RGBA", (width + name_width, height))
+        named_image.paste(image, (name_width, 0))
+
+        draw = ImageDraw.Draw(named_image)
+        font_size = 50
+        font = ImageFont.truetype("arial.ttf", font_size)
+        try:
+            font = ImageFont.truetype("minecraft_font.ttf", font_size)
+        except OSError:
+            self._log.warning(
+                "Unable to find font minecraft_font.ttf. Falling back to Arial."
+            )
+
+        for i, name in enumerate(names):
+            x = name_width - name_right_padding
+            y = (i * padding) + ((i + 1) * (head_height)) - (head_height * 0.5)
+            color_fg = (255, 255, 255)
+            color_outline = (0, 0, 0)
+
+            if self._draw_shadow:
+                draw.text(
+                    (x - 5, y - 5), name, font=font, fill=color_outline, anchor="rm"
+                )
+                draw.text(
+                    (x + 5, y - 5), name, font=font, fill=color_outline, anchor="rm"
+                )
+                draw.text(
+                    (x - 5, y + 5), name, font=font, fill=color_outline, anchor="rm"
+                )
+                draw.text(
+                    (x + 5, y + 5), name, font=font, fill=color_outline, anchor="rm"
+                )
+
+            draw.text((x, y), name, color_fg, font=font, anchor="rm")
+
+        return named_image
+
+    def run_player_image_generator(self):
+        while True:
+            player_image = self._build_player_image_board()
+            if player_image:
+                self._save_image(player_image, self._player_image_name)
+                self._log.info("Saving image: {}".format(self._player_image_name))
+            else:
+                self._log.info("No players found. Not updating image.")
+
+            time.sleep(self._refresh_every_seconds)
+
+
+def get_config(filename, default_params):
+    config = configparser.ConfigParser(default_params)
+    if os.path.isfile(filename):
+        with open(filename, "r") as f:
+            config.read_file(f)
+    else:
+        with open(filename, "w") as f:
+            config.write(f)
+
+    return config
+
+
+def init_log(args):
+    logging.basicConfig(
+        level=args.loglevel.upper(),
+        stream=sys.stdout,
+        format="[%(asctime)s] %(levelname)8s : %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-log",
+        "--loglevel",
+        default="info",
+        help="Provide logging level. Example --loglevel debug, default=info",
+    )
+
+    parser.add_argument(
+        "--config",
+        default="Minecraft_Online_Players_Overlay_Settings.ini",
+        help="Path to config file. Example --config my_custom_config.ini, default=Minecraft_Online_Players_Overlay_Settings.ini",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    parser_args = get_args()
+
+    init_log(parser_args)
+
+    args = {
+        "image_output_dir": ".",
+        "image_name": "online_players.png",
+        "minecraft_server_ip": "mc.3411heavenmedia.com",
+        "minecraft_server_port": 25565,
+        "draw_shadow": True,
+        "vertical_padding": 12,
+        "refresh_every_seconds": 10,
+    }
+
+    config = get_config(parser_args.config, args)
+
+    runtime_args = dict(config.items("DEFAULT"))
+
+    overlay = MinecraftOnlinePlayersOverlay(**runtime_args)
+
+    try:
+        while True:
+            overlay.run_player_image_generator()
+    except KeyboardInterrupt:
+        print("Exiting")
+
+    overlay._mcserver.cleanup()
---
+++ mcserver_connector.py
@@ -0,0 +1,104 @@
+import sys
+import os
+import shutil
+import requests
+from mcstatus import MinecraftServer
+import logging
+import tempfile
+
+
+class MinecraftConnector:
+    MC_HEADS_GET_HEAD = "https://mc-heads.net/avatar/{}.png"
+
+    def __init__(self, server_ip: str, server_port: int = 25565):
+        self._server_ip = server_ip
+        self._server_port = server_port
+        self._temp_dir = self._get_temp_dir()
+
+        self._log = logging.getLogger(__name__ + ".MinecraftConnector")
+
+        self._init_logging()
+
+        self._server = MinecraftServer(self._server_ip, self._server_port)
+
+    def cleanup(self):
+        logging.debug("Removing temp dir: {}".format(self._temp_dir))
+        shutil.rmtree(self._temp_dir)
+
+    def _init_logging(self):
+        self._log.info("Init Minecraft Connector")
+        self._log.info("{}{:<30}{}".format(" " * 4, "Server IP:", self._server_ip))
+        self._log.info("{}{:<30}{}".format(" " * 4, "Server Port:", self._server_port))
+        self._log.info("{}{:<30}{}".format(" " * 4, "Temp Dir", self._temp_dir))
+
+    def _get_temp_dir(self):
+        return tempfile.mkdtemp(prefix="mcconnector_")
+
+    def _status(self):
+        try:
+            return self._server.status()
+        except Exception as e:
+            self._log.error(
+                "Unable to connect to server {}. "
+                "Will continue running in-case it comes online.".format(
+                    "{}:{}".format(self._server_ip, self._server_port)
+                )
+            )
+            return None
+
+    def _get_head_image(self, uuid: str):
+        """
+        Returns file path to the head image.
+        Will download the image every time. In the future,
+        some sort of cache or existence check would be neat.
+        """
+        request_url = MinecraftConnector.MC_HEADS_GET_HEAD.format(uuid)
+        logging.debug("Request head for UUID: {}".format(uuid))
+        logging.debug("Making request to {}".format(request_url))
+        r = requests.get(request_url, stream=True)
+
+        if r.status_code == 200:
+            filepath = os.path.join(self._temp_dir, "{}.png".format(uuid))
+            logging.debug("Set image filepath to: {}".format(filepath))
+            with open(filepath, "wb") as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+
+            return filepath
+        else:
+            return None
+
+    def _get_online_players(self):
+        """
+        @return [{id: str, name: str}, ...]
+        """
+        status = self._status()
+
+        if status:
+            status = status.raw
+            if "sample" not in status["players"]:
+                self._log.fatal(
+                    "Server did not return any player information! Unable to proceed."
+                )
+                raise SystemExit
+
+            players = [p for p in status["players"]["sample"]]
+
+            self._log.debug("{} players found.".format(len(players)))
+
+            for p in players:
+                head = None
+
+                head = self._get_head_image(p["id"])
+
+                p["image"] = head
+
+            return sorted(players, key=lambda p: p["name"])
+        else:
+            return []
+
+
+if __name__ == "__main__":
+    m = MinecraftConnector("mc.3411.best")
+    print(m._get_online_players())
+    # m.cleanup()
